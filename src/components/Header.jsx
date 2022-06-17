e0640cdbcd10874053dc6585a50dea9c0502365e
Ainur <ainura.abdenova@gmail.com>
2022-06-17 22:59:53 +0600

    create-components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div>
+            <header className='header'>
+                <img src='https://www.logolynx.com/images/logolynx/a6/a6ad443002c8be305238154585a07804.jpeg' />
+            </header>
+        </div>
+    )
+}
